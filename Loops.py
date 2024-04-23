0e2eca6cf4f89e9122d102f44859cd1f40f9841b
Bolundut Razvan-Mihai <razvan_mihai_2010@yahoo.com>
2024-04-23 15:39:08 +0300

    User context generation for loop - FOR

--- Loops.py
+++ Loops.py
@@ -1,31 +1,33 @@
 from string import Template
 import random
 from wonderwords import RandomWord
+from typing import Dict
 
-from util import C_DATA_TYPES
+from util import C_DATA_TYPES, ContextualTemplateObject
 
 #...For loop...
-class For:
+class For(ContextualTemplateObject):
     TEMPLATE_FILE = 'snippets/for.template'
+    VALID_REGEX = '^(?!auto$|break$|case$|char$|const$|continue$|default$|do$|double$|else$|enum$|extern$|float$|for$|goto$|if$|int$|long$|register$|return$|short$|signed$|sizeof$|static$|struct$|switch$|typedef$|union$|unsigned$|void$|volatile$|while$)(?!.*-)[a-zA-Z]+$'
 
     def __init__(self):
         with open(self.TEMPLATE_FILE, 'r') as templ_file: 
             self.template_string = templ_file.read()
         
-        self.counter_variable_name = RandomWord().word()
+        self.counter_variable_name = RandomWord().word(regex=self.VALID_REGEX)
         self.counter_initial_val = C_DATA_TYPES['int']()
         self.counter_target_val = C_DATA_TYPES['int']()
         while self.counter_initial_val == self.counter_target_val:
             self.counter_target_val = C_DATA_TYPES['int']()
         self.couter_step_increment = '++' if self.counter_initial_val < self.counter_target_val else '--'
         self.couter_step_condition_sign = '<=' if self.counter_initial_val < self.counter_target_val else '>='
+        self.loop_body = None
 
     def get_loop_symbol_table(self):
         return {self.counter_variable_name: 'int'}
 
-    def set_body(self, body):
-        t = Template(self.template_string)
-        self.template_string = t.safe_substitute(loop_body = str(body))
+    def set_body(self, body: ContextualTemplateObject):
+        self.loop_body = body
         return self
 
     def change_loop_counter_name(self, name):
@@ -41,14 +43,18 @@ class For:
     def get_loop_increment_instruction(self):
         return self.counter_variable_name + self.couter_step_increment;
 
-    def __str__(self):
+    def print_contextual(self, symbol_table: Dict[str, str] = None) -> str:
         return Template(self.template_string)\
             .safe_substitute(\
                 loop_init = self.get_loop_initializer_instruction(),\
                 loop_condition = self.get_loop_condition_instruction(),\
-                loop_post_instr = self.get_loop_increment_instruction())
+                loop_post_instr = self.get_loop_increment_instruction(),\
+                loop_body = self.loop_body.print_contextual(symbol_table))
+
+#................
 
 #...While loop...
+
 class While:
     TEMPLATE_FILE = 'snippets/while.template'
 
@@ -65,3 +71,5 @@ class While:
 
     def __str__(self):
         return self.template_string
+
+#................
